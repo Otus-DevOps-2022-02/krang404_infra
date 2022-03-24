7dcc88dd92cec9d3c6380de14a8ad287f0db76fe
Nkilay Krasnov <kra-nik@yandex.ru>
2022-03-24 19:46:09 +1000

    Fix errors install mongodb and deploy app

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install git
+cd /home/appuser/
 sudo git clone -b monolith https://github.com/express42/reddit.git
-cd ~/reddit && bundle install
+cd /home/appuser/reddit && bundle install
 puma -d
