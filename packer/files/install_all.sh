5d1be094d8ef0cdfd5bd5ddcda9213fc3ca22fef
Nkilay Krasnov <kra-nik@yandex.ru>
2022-04-01 01:13:06 +1000

    All project to create bake and fry image

---
+++ packer/files/install_all.sh
@@ -0,0 +1,9 @@
+
+#!/bin/sh
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt update -y
+sleep 5s
+apt install -y mongodb-org ruby-full ruby-bundler build-essential git
+systemctl start mongod
+systemctl enable mongod
