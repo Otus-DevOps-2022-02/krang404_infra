a64bd949319992ecadcd8a63ee2db5e7b914ecad
Nkilay Krasnov <kra-nik@yandex.ru>
2022-04-10 11:57:14 +1000

    Fix errors in terraform.tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,10 @@
 variable "cloud_id" {
   description = "Cloud"
   type        = string
-  default     = "b1g23e813tdf5mi1dlqg"
 }
 variable "folder_id" {
   description = "Folder"
   type        = string
-  default     = "b1g3ev1fmlsskdbgvue4"
 }
 #variable "ya_auth_token" {
 #  description = "ya_auth_token"
@@ -20,25 +18,20 @@ variable "zone" {
 variable "public_key_path" {
   # Описание переменной
   description = "Path to the public key used for ssh access"
-  default     = "~/.ssh/yc.pub"
 }
 variable "private_key_path" {
   description = "Path to the private key used for ssh access"
-  default     = "~/.ssh/yc"
 }
 variable "image_id" {
   description = "Disk image"
   type        = string
-  default     = "fd8rboq00lf9v4n4v033"
 }
 variable "subnet_id" {
   description = "Subnet"
   type        = string
-  default     = "e9b21gs2blhef5ec7ke7"
 }
 variable "service_account_key_file" {
   description = "key.json"
-  default     = "/home/nik/Otus/GitHub-lab/key.json"
 }
 variable "instance_count" {
   description = "count var"
