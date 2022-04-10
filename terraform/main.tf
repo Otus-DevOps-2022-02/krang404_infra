3a5b20976e586b6e480635eb262209f26f6070e3
Nkilay Krasnov <kra-nik@yandex.ru>
2022-04-10 12:07:20 +1000

    Fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = ">= 0.35"
+      version = "~> 0.35"
     }
   }
-  required_version = ">= 0.12.0"
+  required_version = ">= 1.00"
 }
 
 provider "yandex" {
