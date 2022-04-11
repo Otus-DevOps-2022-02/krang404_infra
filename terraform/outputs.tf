ecb07a98dac116154a5f8433678a7f03f9b4f786
Nkilay Krasnov <kra-nik@yandex.ru>
2022-04-11 20:28:13 +1000

    1st commit terraform-2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,8 @@
 output "external_ip_address_app" {
-  value = yandex_compute_instance.app[0].network_interface.0.nat_ip_address
+  value = yandex_compute_instance.app.network_interface.0.nat_ip_address
+}
+output "external_ip_address_db" {
+  value = yandex_compute_instance.db.network_interface.0.nat_ip_address
 }
 output "external_ip_address_load_balancer" {
   value = yandex_lb_network_load_balancer.lb_reddit.listener.*.external_address_spec[0].*.address
